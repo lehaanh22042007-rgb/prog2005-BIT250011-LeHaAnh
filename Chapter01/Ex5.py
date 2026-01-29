14b20c22ecdce68e2d644a0443e4438f20d8663f
lehaanh22042007-rgb <lehaanh22042007@gmail.com>
2026-01-29 23:13:12 +0700

    Create Ex5.py

---
+++ Chapter01/Ex5.py
@@ -0,0 +1,11 @@
+#bài 5
+name = "lê hà anh"
+age = int(18)
+average_score = float(36)
+print(type(name))
+print(type(age))
+print(type(average_score))
+age_next_year = age + 1
+doubled_score = average_score * 2
+print(type(doubled_score))
+print(type(age_next_year))
