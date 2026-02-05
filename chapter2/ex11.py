03c92377d51b92713a299bae365dd8fef495a4a7
lehaanh22042007-rgb <lehaanh22042007@gmail.com>
2026-02-05 13:48:13 +0700

    Create ex11.py

---
+++ chapter2/ex11.py
@@ -0,0 +1,10 @@
+#bài 11
+diem = float(input("mời nhập điểm trung bình: "))
+if diem >= 8:
+    print("Xếp loại: Giỏi")
+elif diem >= 6.5:
+    print("Xếp loại: Khá")
+elif diem >= 5:
+    print("Xếp loại: Trung bình")
+else:
+    print("Xếp loại: Yếu")
