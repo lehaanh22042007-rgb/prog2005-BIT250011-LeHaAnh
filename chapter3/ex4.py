dc67a65f8926065f3304a4c775e0d499369b3621
lehaanh22042007-rgb <lehaanh22042007@gmail.com>
2026-03-05 13:32:55 +0700

    Add sorting, reversing, and counting to ex4.py

---
+++ chapter3/ex4.py
@@ -0,0 +1,11 @@
+#bài 4
+so_dep = [5,11,7,23,17,19]
+# sắp xếp thứ tự tăng dần
+so_dep.sort()
+print(so_dep)
+# đảo ngược
+so_dep.reverse()
+print(so_dep)
+# đếm số
+so_dep.count(5)
+print(so_dep.count(5))
