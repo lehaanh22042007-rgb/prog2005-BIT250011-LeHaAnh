acedd54c56d7bdcb81744e4eba6ab7917a28baf6
lehaanh22042007-rgb <lehaanh22042007@gmail.com>
2026-03-13 17:21:57 +0700

    Add ex2.py for basic arithmetic operations

---
+++ bai tap luyen tap1/ex2.py
@@ -0,0 +1,9 @@
+#bài 2
+a = float(input("a="))
+b = float(input("b="))
+print(a,"^",b,"=",a**b)
+print("căn bậc hai của ",a,"=",a**0.5)
+print("căn bậc 2 của ",b,"=",b**0.5)
+print(a,"chia ",b,"lấy phần nguyên =",a//b)
+print(a,"chia ",b,"lấy phần dư =",a%b)
+print(a,"sau khi làm tròn:",a.__round__())
