6b7fa65c24e308e8cc029c092d358c2cf92bd6f6
lehaanh22042007-rgb <lehaanh22042007@gmail.com>
2026-03-20 20:37:12 +0700

    Create ex1.py

---
+++ chapter5/ex1.py
@@ -0,0 +1,9 @@
+#bài 1
+import matplotlib.pyplot as plt
+labels = ["Xuất sắc", "Giỏi", "Trung bình", "Yếu", "Kém"]
+values = [6, 10, 12, 4, 1]
+plt.bar(labels, values)
+plt.title("Kết quả học tập của lớp")
+plt.xlabel("Xếp loại")
+plt.ylabel("Số lượng học sinh")
+plt.show()
